cb434e287cd65c9c8b6d9b985dce475d26f6a74d
frieburd <bfozard@gmail.com>
2021-08-18 08:48:32 -0700

    Update generate-zip.sh

--- generate-zip.sh
+++ generate-zip.sh
@@ -4,5 +4,5 @@ set -x
 VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`
 
 sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml > install.xml
-zip -r SqueezeCloud-$VERSION.zip . -x \*.zip \*.sh \*.git\* \*README\* \*webauth\* \*.sublime\* \*.DS_Store\* \*.editorconfig \*.template.xml
+zip -r MiniDspShd-$VERSION.zip . -x \*.zip \*.sh \*.git\* \*README\* \*webauth\* \*.sublime\* \*.DS_Store\* \*.editorconfig \*.template.xml
 rm install.xml
